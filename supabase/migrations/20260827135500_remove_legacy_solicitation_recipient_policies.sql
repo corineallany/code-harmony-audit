93daf45c17f5000a93759508ccda3fccec697376
corineallany <corineallany@gmail.com>
2026-08-27 13:58:55 +0200

    chore: supprimer anciennes policies sollicitations

---
+++ supabase/migrations/20260827135500_remove_legacy_solicitation_recipient_policies.sql
@@ -0,0 +1,3 @@
+drop policy if exists solrec_write_staff on public.solicitation_recipients;
+drop policy if exists solicitation_recipients_read_authenticated on public.solicitation_recipients;
+drop policy if exists solrec_select on public.solicitation_recipients;
